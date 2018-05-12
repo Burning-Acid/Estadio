0b56bd2c4a6eddb1dcd8bc702103fd4e1ea5d978
Burning-Acid <dorian.r.mv@gmail.com>
2018-05-12 11:17:50 -0500

    Primer Commit!!!!
    Este es el primer commit que hacemos con el proyecto!

---
+++ Proyecto3/src/controlador/Controlador.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import controllers.*;
+import entities.*;
+import vista.*;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author doria
+ */
+public class Controlador {
+    
+        //Creación del Entity Manager Factory
+        private final EntityManagerFactory emf;
+        
+        //Creación de los controladores:
+        
+        private static ClubJpaController controClub;
+        private static ContinenteJpaController controContinente;
+        private static EntrenadorJpaController controEntrenador;
+        private static EquipoPaisJpaController controEquipoPais;
+        private static EstadioJpaController controEstadio;
+        private static GolJpaController controGol;
+        private static GrupoJpaController controGrupo;
+        private static JuezJpaController controJuez;
+        private static JugadorJpaController controJugador;
+        private static PaisJpaController controPais;
+        private static PartidoJpaController controPartido;
+        private static PosicionJpaController controPosicion;
+        private static RolJpaController controRol;
+        private static SillaJpaController controSilla;
+        private static TarjetaJpaController controTarjeta;
+        private static UsuarioJpaController controUsuario;
+        private Ventana vista;
+        
+        Controlador()
+        {
+            //Creación del Entity Manager Factory
+            emf = Persistence.createEntityManagerFactory("ControladorPU");
+
+            //Creación de los controladores:
+
+            controClub = new ClubJpaController(emf);
+            controContinente = new ContinenteJpaController(emf);
+            controEntrenador = new EntrenadorJpaController(emf);
+            controEquipoPais = new EquipoPaisJpaController(emf);
+            controEstadio = new EstadioJpaController(emf);
+            controGol = new GolJpaController(emf);
+            controGrupo = new GrupoJpaController(emf);
+            controJuez = new JuezJpaController(emf);
+            controJugador = new JugadorJpaController(emf);
+            controPais = new PaisJpaController(emf);
+            controPartido = new PartidoJpaController(emf);
+            controPosicion = new PosicionJpaController(emf);
+            controRol = new RolJpaController(emf);
+            controSilla = new SillaJpaController(emf);
+            controTarjeta = new TarjetaJpaController(emf);
+            controUsuario = new UsuarioJpaController(emf);
+            
+            vista = new Ventana(this);
+            vista.setVisible(true);
+        }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Controlador controlador = new Controlador();
+    }
+    
+}
